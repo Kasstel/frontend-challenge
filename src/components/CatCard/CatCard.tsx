d4334c0b72bb0b7076ab6875001872a2483a80c0
Kasstel <kasstel03@yandex.ru>
2026-04-09 18:00:44 +0300

    fix:hovered-heart

--- src/components/CatCard/CatCard.tsx
+++ src/components/CatCard/CatCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import type { Cat } from '../../App'
 import { HeartIcon } from '../HeartIcon'
 import styles from './CatCard.module.css'
@@ -9,12 +10,14 @@ interface CatCardProps {
 }
 
 export function CatCard({ cat, isFavorite, onToggleLike }: CatCardProps) {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <>
       <article className={styles.card}>
         <img src={cat.url} className={styles.image} />
-        <button className={styles.likeButton} onClick={onToggleLike}>
-          <HeartIcon filled={isFavorite} />
+        <button className={styles.likeButton} onClick={onToggleLike} onMouseEnter={()=>{setHovered(true)}} onMouseLeave={()=>{setHovered(false)}}>
+          <HeartIcon filled={isFavorite || hovered} />
         </button>
       </article>
     </>
